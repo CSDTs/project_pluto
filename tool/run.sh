f2fd24363aea2e3d9c8b48d9903f9dcd9cb7c811
Charles Hathaway <chathaway@logrit.com>
2013-12-30 23:32:48 -0500

    Added in the downloading of content_shell

--- tool/run.sh
+++ tool/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+$DART_SDK/../chromium/download_contentshell.sh
+unzip content_shell-linux-x64-release.zip
+mv drt*/* .
 sudo start xvfb
 dart tool/hop_runner.dart test_js_codelet
 dart tool/hop_runner.dart test_js_variable
